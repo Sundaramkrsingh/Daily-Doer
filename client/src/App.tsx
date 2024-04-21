0b4aa64bae70cafb519cbecef7bb7f2de5c5544c
Sundaram Singh <sundaramsinghsdnr@gmail.com>
2024-04-21 19:48:09 +0530

    added routes for view edit todo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,10 @@ import { Route, Routes } from "react-router-dom"
 import SignUp from "./pages/signup"
 import Signin from "./pages/signin"
 import Todos from "./pages/todos"
-import Todo from "./pages/todo"
 import LandingPage from "./pages/landing"
-import AddTodo from "./pages/addTodo"
+import AddTodo from "./pages/todoAdd"
+import TodoEdit from "./pages/todoEdit"
+import TodoView from "./pages/todoView"
 
 function App() {
 
@@ -14,8 +15,10 @@ function App() {
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/todos" element={<Todos />} />
-      <Route path="/todo" element={<Todo />} />
       <Route path="/todo/add" element={<AddTodo />} />
+      <Route path="/todo/edit/:todoId" element={<TodoEdit />} />
+      <Route path="/todo/view/:todoId" element={<TodoView />} />
+      <Route path="*" element={<div>Not Found 404</div>} />
     </Routes>
   </>
 }
